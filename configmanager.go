67da757c021228e881388eadd0e0160b8d3c6e39
PulsarG <pulsar.g.projects@gmail.com>
2023-05-10 14:28:54 +0500

    first commit

---
+++ configmanager.go
@@ -0,0 +1,32 @@
+// HANDLER CONFIG FILE
+// READ, WHRITE, TAKE
+
+package inihandler
+
+import (
+	"fmt"
+	"github.com/go-ini/ini"
+)
+
+func cfgHandler() *ini.File {
+	cfg, err := ini.Load("config.ini")
+	if err != nil {
+		fmt.Printf("Error loading config file: %s\n", err)
+		return nil
+	}
+	return cfg
+}
+
+func GetFromIni(section, key string) string {
+	return cfgHandler().Section(section).Key(key).String()
+}
+
+func SaveToIni(section, key, val string) {
+	cfg := cfgHandler()
+	cfg.Section(section).Key(key).SetValue(val)
+	err := cfg.SaveTo("config.ini")
+	if err != nil {
+		fmt.Printf("Error saving config file: %s\n", err)
+		return
+	}
+}
